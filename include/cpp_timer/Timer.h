f0f2dc3d243d887304cb825178cbe27882aeca23
alexnavtt <alexnavtt@utexas.edu>
2021-05-26 13:15:59 -0500

    Added lisence

--- include/cpp_timer/Timer.h
+++ include/cpp_timer/Timer.h
@@ -1,3 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+//      Title     : Timer.h
+//      Project   :	cpp_timer
+//      Author    : Alex Navarro
+//      Created   : 05/25/2021
+//      Copyright : Copyright© The University of Texas at Austin, 2014-2021. All rights reserved.
+//                
+//          All files within this directory are subject to the following, unless an alternative
+//          license is explicitly included within the text of each file.
+//
+//          This software and documentation constitute an unpublished work
+//          and contain valuable trade secrets and proprietary information
+//          belonging to the University. None of the foregoing material may be
+//          copied or duplicated or disclosed without the express, written
+//          permission of the University. THE UNIVERSITY EXPRESSLY DISCLAIMS ANY
+//          AND ALL WARRANTIES CONCERNING THIS SOFTWARE AND DOCUMENTATION,
+//          INCLUDING ANY WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//          PARTICULAR PURPOSE, AND WARRANTIES OF PERFORMANCE, AND ANY WARRANTY
+//          THAT MIGHT OTHERWISE ARISE FROM COURSE OF DEALING OR USAGE OF TRADE.
+//          NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH RESPECT TO THE USE OF
+//          THE SOFTWARE OR DOCUMENTATION. Under no circumstances shall the
+//          University be liable for incidental, special, indirect, direct or
+//          consequential damages or loss of profits, interruption of business,
+//          or related expenses which may arise from use of software or documentation,
+//          including but not limited to those resulting from defects in software
+//          and/or documentation, or loss or inaccuracy of data of any kind.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
 #ifndef COVERAGE_PLANNER_TIMER_H_
 #define COVERAGE_PLANNER_TIMER_H_
 
@@ -15,14 +45,16 @@ namespace cpp_timer{
 // Forward declaration
 struct Layer;
 
+// Typedefs for readability
 typedef std::shared_ptr<cpp_timer::Layer> LayerPtr;
 typedef std::chrono::high_resolution_clock::time_point chronoTime;
-typedef std::map<std::string, double> durationMap;
+typedef std::map<std::string, long int> durationMap;
+typedef std::map<std::string, LayerPtr> layerMap;
 
 struct Layer{
     int layer_index;
-    std::map<std::string, long int> durations;
-    std::map<std::string, LayerPtr> children;
+    durationMap durations;
+    layerMap children;
     LayerPtr parent;
 };
 
@@ -80,7 +112,7 @@ private:
     /**
      * Get the total time spent in a layer through recursive calculation
      */
-    std::map<std::string, double> getTotals_(LayerPtr layer);
+    durationMap getTotals_(LayerPtr layer);
 };
 
 }   // namespace cpp_timer
--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,3 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+//      Title     : Timer.cpp
+//      Project   :	cpp_timer
+//      Author    : Alex Navarro
+//      Created   : 05/25/2021
+//      Copyright : Copyright© The University of Texas at Austin, 2014-2021. All rights reserved.
+//                
+//          All files within this directory are subject to the following, unless an alternative
+//          license is explicitly included within the text of each file.
+//
+//          This software and documentation constitute an unpublished work
+//          and contain valuable trade secrets and proprietary information
+//          belonging to the University. None of the foregoing material may be
+//          copied or duplicated or disclosed without the express, written
+//          permission of the University. THE UNIVERSITY EXPRESSLY DISCLAIMS ANY
+//          AND ALL WARRANTIES CONCERNING THIS SOFTWARE AND DOCUMENTATION,
+//          INCLUDING ANY WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//          PARTICULAR PURPOSE, AND WARRANTIES OF PERFORMANCE, AND ANY WARRANTY
+//          THAT MIGHT OTHERWISE ARISE FROM COURSE OF DEALING OR USAGE OF TRADE.
+//          NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH RESPECT TO THE USE OF
+//          THE SOFTWARE OR DOCUMENTATION. Under no circumstances shall the
+//          University be liable for incidental, special, indirect, direct or
+//          consequential damages or loss of profits, interruption of business,
+//          or related expenses which may arise from use of software or documentation,
+//          including but not limited to those resulting from defects in software
+//          and/or documentation, or loss or inaccuracy of data of any kind.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include "cpp_timer/Timer.h"
 
 using std::string;
@@ -74,7 +103,7 @@ void Timer::summary(){
 
     // Show the total time spent on each function, regardless of parent/child times
     std::cout << "\n===== SUMMARY =====\n";
-    std::map<std::string, double> totals = getTotals_(current_layer_);
+    durationMap totals = getTotals_(current_layer_);
     for (std::pair<std::string, double> p : totals){
         std::string name = p.first;
         int duration     = round(p.second/1000);
