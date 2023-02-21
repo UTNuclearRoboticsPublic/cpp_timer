6edd0f914ab5ca6a62cbbacc99c15e61f2368611
alexnavtt <alexnavtt@utexas.edu>
2023-02-21 15:35:06 -0600

    updated missed typedef

--- include/cpp_timer/typedefs.h
+++ include/cpp_timer/typedefs.h
@@ -15,7 +15,7 @@ class Ticker;
 // Typedefs for readability
 typedef std::shared_ptr<cpp_timer::Layer> LayerPtr;
 typedef std::chrono::steady_clock::time_point chronoTime;
-typedef std::chrono::duration<long int, std::nano> chronoDuration;
+typedef std::chrono::duration<std::chrono::steady_clock::rep, std::nano> chronoDuration;
 typedef std::map<std::string_view, chronoDuration> durationMap;
 typedef std::map<std::string_view, LayerPtr> layerMap;
 typedef std::map<std::string_view, std::pair<int, chronoDuration>> timerTotal;
